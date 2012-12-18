bce4855151e5ba3e4476a074737a89223f4e431f
Gary McGhee <gary@buzzware.com.au>
2012-12-18 14:30:46 +0800

    work on KeyChain_spec.rb

--- lib/dash/key_chain.rb
+++ lib/dash/key_chain.rb
@@ -6,12 +6,27 @@ module Dash
 			@namespace = aNamespace
 		end
 
-		def set(aKey,aValue)
-			@keychain[@namespace,aKey] = aValue
+		def set(aKey,aValue=nil,aPrefix='')
+			if (aKey.is_a?(String) || aKey.is_a?(Symbol))
+				@keychain[@namespace,aPrefix.to_s+aKey.to_s] = aValue
+			elsif aKey.is_a?(Object)
+				prefix = aValue || aPrefix
+				aKey.each {|k,v| set(k,v,prefix)}
+			end
 		end
 
-		def get(aKey)
-			@keychain[@namespace,aKey]
+		def get(aKey,aPrefix=nil,aKeepPrefix=false)
+			if (aKey.is_a?(Array))
+				result = {}
+				aKey.each do |k|
+					storeKey = (aKeepPrefix ? aPrefix.to_s+k.to_s : k.to_s)
+					v = get(k.to_s,aPrefix)
+					result[storeKey] = v
+				end
+				return result
+			else
+				return @keychain[@namespace,aPrefix.to_s+aKey.to_s]
+			end
 		end
 	end
 end
--- spec/KeyChain_spec.rb
+++ spec/KeyChain_spec.rb
@@ -15,4 +15,50 @@ describe "KeyChain" do
 		readValue = keyChain.get('testKey')
 		readValue.should == value
 	end
+
+	it "should support get and set with a prefix" do
+		value1 = StringUtils.random_word(8,8)
+		value2 = StringUtils.random_word(8,8)
+		keyChain.set('testKey',value1)
+		keyChain.set('testKey',value2,'blah')
+		keyChain.get('testKey').should==value1
+		keyChain.get('testKey','blah').should==value2
+	end
+
+	it "should support multiple get" do
+		value1 = StringUtils.random_word(8,8)
+		value2 = StringUtils.random_word(8,8)
+		keyChain.set('testKey1',value1)
+		keyChain.set('testKey2',value2)
+		keyChain.get('testKey1').should==value1
+		keyChain.get('testKey2').should==value2
+		keyChain.get(['testKey1']).should=={'testKey1' => value1}
+		keyChain.get(['testKey2']).should=={'testKey2' => value2}
+		keyChain.get(['testKey1','testKey2']).should=={'testKey1' => value1,'testKey2' => value2}
+		keyChain.get(%w(testKey1 testKey2)).should=={'testKey1' => value1,'testKey2' => value2}
+	end
+
+	it "should support multiple get with a prefix" do
+		value1 = StringUtils.random_word(8,8)
+		value2 = StringUtils.random_word(8,8)
+		keyChain.set('testKey1','')
+		keyChain.set('testKey2','')
+		keyChain.set('testKey1',value1,'prefix1.')
+		keyChain.set('testKey2',value2,'prefix1.')
+		keyChain.get(%w(testKey1 testKey2)).should == {'testKey1' => '', 'testKey2' => ''}
+		keyChain.get(%w(testKey1 testKey2),'prefix1.').should == {'testKey1' => value1, 'testKey2' => value2}
+		keyChain.get(%w(testKey1 testKey2),'prefix1.',true).should == {'prefix1.testKey1' => value1, 'prefix1.testKey2' => value2}
+	end
+
+	it "should support multiple set" do
+		value1 = StringUtils.random_word(8,8)
+		value2 = StringUtils.random_word(8,8)
+		keyChain.set({
+			'testKey1' => value1,
+		  'testKey2' => value2
+		})
+		keyChain.get('testKey1').should==value1
+		keyChain.get('testKey2').should==value2
+	end
+
 end
